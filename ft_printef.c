1281559a5f1ced146b219416cd8a144956a3fdd1
Artur HRYTSENKO <ahrytsen@e2r12p14.unit.ua>
2017-11-30 13:15:27 +0200

    start

---
+++ ft_printef.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/29 16:08:02 by ahrytsen          #+#    #+#             */
+/*   Updated: 2017/11/29 21:34:24 by ahrytsen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+
+int		printf(const char * restrict format, ...)
+{
+	int		symbols;
+	va_list	arg;
+
+	symbols = 0;
+	va_start(arg, format);
+
+}
+
+
+
+
+
+
+
+
+
+
