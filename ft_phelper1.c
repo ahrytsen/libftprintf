342f3f89723bd59dfbc2d325454bfa8c30b3485b
Артур <motofun@MacBook-Pro-Artur.local>
2017-12-23 05:07:27 +0200

    %d fix

--- ft_phelper1.c
+++ ft_phelper1.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 14:55:20 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/12/22 20:45:46 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/12/23 05:06:34 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,56 +60,59 @@ void		ft_str(va_list *ap, t_arg *arg)
 
 void	ft_int(va_list *ap, t_arg *arg)
 {
-	char	*sign;
-	char	*snbr;
-	char	*prec;
-	int		len;
+	char	*tmp[3];
+	int		len[2];
 
-	snbr = ft_ltoa(ft_getnbr(ap, arg));
-	sign = (*snbr == '-') ? "-" : "";
-	(ft_strchr(arg->flags, '+') && !*sign) ? sign = "+" : 0;
-	(ft_strchr(arg->flags, ' ') && !*sign) ? sign = " " : 0;
-	len = ft_strlen(snbr);
-	(*snbr == '-' || (arg->is_prec && !arg->prec && *snbr == '0')) ? len-- : 0;
-	prec = arg->is_prec && arg->prec > len
-		? ft_memalloc(arg->prec - len + 1) : NULL;
-	prec ? ft_memset(prec, '0', arg->prec - len) : 0;
-	(sign = ft_strjoin(sign, prec)) ? free(prec) : 0;
-	if (ft_strchr(arg->flags, '-') || arg->width < 0
-		|| (!ft_strchr(arg->flags, '-') && arg->width >= 0
-			&& ft_strchr(arg->flags, '0') && !arg->is_prec))
-		ft_putstr_buf(sign, ft_strlen(sign));
-	if ((len += ft_strlen(sign)) < MOD(arg->width))
-		ft_filler(arg, MOD(arg->width) - len);
-	if ((!ft_strchr(arg->flags, '-') && arg->width >= 0)
-			|| !ft_strchr(arg->flags, '0') && !arg->is_prec))
-		ft_putstr_buf(sign, ft_strlen(sign));
-	ft_putstr_buf(snbr + (*snbr == '-' ? 1 : 0), len);
-	if (!ft_strchr(arg->flags, '-') && arg->width >= 0)
-		(arg->spec == 'S' || (arg->len && !ft_strcmp(arg->len, "l")))
-			? ft_putustr_buf(s, len) : ft_putstr_buf(s, len);
-	free(snbr);
+	tmp[2] = ft_ltoa(ft_getnbr(ap, arg));
+	tmp[0] = (*tmp[2] == '-') ? "-" : NULL;
+	(ft_strchr(arg->flags, '+') && !tmp[0]) ? tmp[0] = "+" : 0;
+	(ft_strchr(arg->flags, ' ') && !tmp[0]) ? tmp[0] = " " : 0;
+	len[0] = ft_strlen(tmp[2]);
+	if (*tmp[2] == '-' || (arg->is_prec && !arg->prec && *tmp[2] == '0'))
+		len[0]--;
+	tmp[1] = arg->is_prec && arg->prec > len[0]
+		? ft_memalloc(arg->prec - len[0] + 1) : NULL;
+	tmp[1] ? ft_memset(tmp[1], '0', arg->prec - len[0]) : 0;
+	(tmp[0] = ft_strjoin(tmp[0], tmp[1])) ? free(tmp[1]) : 0;
+	len[1] = MOD(arg->width) - len[0] - (tmp[0] ? ft_strlen(tmp[0]) : 0);
+	(arg->width >= 0 && (arg->is_prec || !ft_strchr(arg->flags, '0'))
+	 && !ft_strchr(arg->flags, '-') && len[1] > 0) ? ft_filler(arg, len[1]) : 0;
+	tmp[0] ? ft_putstr_buf(tmp[0], ft_strlen(tmp[0])) : 0;
+	free(tmp[0]);
+	(!ft_strchr(arg->flags, '-') && arg->width >= 0 && len[1] > 0
+	 && ft_strchr(arg->flags, '0') && !arg->is_prec) ? ft_filler(arg, len[1]) : 0;
+	ft_putstr_buf(tmp[2] + (*tmp[2] == '-' ? 1 : 0), len[0]);
+	free(tmp[2]);
+	if ((ft_strchr(arg->flags, '-') || arg->width >= 0) && len[1] > 0)
+		ft_filler(arg, len[1]);
 }
 
 void	ft_base(va_list *ap, t_arg *arg)
 {
-	char	*res;
-	size_t	num;
-//	int		len;
-	char	*pref;
+	char	*tmp[3];
+	int		len[2];
 	int		base;
 
-	num = ft_getnbr(ap, arg);
 	base = (arg->spec == 'X' || arg->spec == 'x' || arg->spec == 'p') ? 16 : 2;
 	base = (arg->spec == 'O' || arg->spec == 'o') ? 8 : base;
-	res = ft_ultoa_base(num, base, 'A' + arg->spec - ft_toupper(arg->spec));
-	pref = (arg->spec == 'X' || arg->spec == 'p') ? "0X" : "b";
-	pref = (arg->spec == 'x') ? "0x" : pref;
-	pref = (arg->spec == 'o' || arg->spec == 'O') ? "0" : pref;
-	if ((num || ft_strchr("oO", arg->spec)) && ft_strchr(arg->flags, '#'))
-		ft_putstr_buf(pref, ft_strlen(pref));
-	(!num  && arg->is_prec && !arg->prec) ? 0 : ft_putstr_buf(res, ft_strlen(res));
-	free(res);
+	tmp[2] = ft_ultoa_base(ft_getnbr(ap, arg), base, arg->spec == 'X' ? 'A' : 'a');
+	tmp[0] = (arg->spec == 'X' || arg->spec == 'p') ? "0X" : NULL;
+	(arg->spec == 'x') ? tmp[0] = "0x" : 0;
+	(arg->spec == 'o' || arg->spec == 'O') ? tmp[0 = ]"0" : 0;
+	len[0] = ft_strlen(tmp[2]);
+	tmp[1] = arg->is_prec && arg->prec > len[0]
+		? ft_memalloc(arg->prec - len[0] + 1) : NULL;
+	tmp[1] ? ft_memset(tmp[1], '0', arg->prec - len[0]) : 0;
+	(tmp[0] = ft_strjoin(tmp[0], tmp[1])) ? free(tmp[1]) : 0;
+	len[1] = MOD(arg->width) - len[0] - (tmp[0] ? ft_strlen(tmp[0]) : 0);
+	(arg->width >= 0 && (arg->is_prec || !ft_strchr(arg->flags, '0'))
+	 && !ft_strchr(arg->flags, '-') && len[1] > 0) ? ft_filler(arg, len[1]) : 0;
+	tmp[0]  ? ft_putstr_buf(tmp[0], ft_strlen(tmp[0])) : 0;
+	free(tmp[0]);
+	(!ft_strchr(arg->flags, '-') && arg->width >= 0 && len[1] > 0
+	 && ft_strchr(arg->flags, '0') && !arg->is_prec) ? ft_filler(arg, len[1]) : 0;
+	ft_putstr_buf(tmp[2] + (*tmp[2] == '-' ? 1 : 0), len[0]);
+	free(tmp[2]);
 }
 
 void	ft_undef(va_list *ap, t_arg *arg)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 13:17:30 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/12/22 20:01:59 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/12/23 03:55:23 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@
 
 int	main()
 {
-	setlocale(LC_ALL, "");
-	long double width = 0;
-	int l = 150;
-	char *s = ft_strdup("some string");
-	printf("\n%d\n", printf("%+010.1d", 2));
-//	printf("\n%d\n", ft_printf("%.2o", 0));
+	printf("\n%d\n", printf("%d", 5));
+	printf("\n%d\n", ft_printf("%4.3d", 65));
 }
